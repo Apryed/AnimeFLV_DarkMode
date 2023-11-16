1a00f081db6fb6573da168a4d35a18a955310b27
Apryed <Apryed@users.noreply.github.com>
2023-11-16 20:14:41 +0100

    Update UserScript.js

--- UserScript.js
+++ UserScript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AnimeFLV - Dark Mode
 // @namespace    https://github.com/Apryed/AnimeFLV_DarkMode
-// @version      2.0
+// @version      2.1
 // @description  Transforms AnimeFLV to Dark Mode!
 // @author       Apryed
 // @match        https://www3.animeflv.net/*
