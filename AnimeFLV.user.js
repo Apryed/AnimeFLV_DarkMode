3218053987c7c9aecb2cbfae13dfa76b5fafe7bf
Apryed <Apryed@users.noreply.github.com>
2023-11-16 20:47:25 +0100

    Update AnimeFLV.user.js
    Removed an unnecesary banner.

--- AnimeFLV.user.js
+++ AnimeFLV.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AnimeFLV - Dark Mode
 // @namespace    https://github.com/Apryed/AnimeFLV_DarkMode
-// @version      2.1.2
+// @version      2.1.3
 // @description  Transforms AnimeFLV to Dark Mode!
 // @author       Apryed
 // @match        https://www3.animeflv.net/*
