f863da7a95a35aff0ea17d1b39b4bbe722f481bd
Apryed <Apryed@users.noreply.github.com>
2023-11-16 20:23:53 +0100

    Update UserScript.js

--- UserScript.js
+++ UserScript.js
@@ -15,13 +15,9 @@
 
 (function() {
     'use strict';
-    let black="#000"
-    let grey="#0A0A0A"
-    let lgrey="#2A2A2A"
-    let blue="#14bbf0"
-    let white="#FFF"
+    let black="#000", grey="#0A0A0A", lgrey="#2A2A2A", blue="#14bbf0", red="#F00", white="#FFF", fb="#4267B2"
 
-    let Style = `.Header,.Footer {background-color:`+grey+`;} #search-anime, h1, h2, a, strong { color:`+white+` !important;} .Body, .WdgtCn, .WdgtCn .Title {background-color:`+black+`;color:`+white+`;} .CapiTnv>li>a {background-color:`+lgrey+`;} .CapiTnv>li.active>a {background-color:`+grey+`;box-shadow: inset 0 2px 0 `+blue+`;} .fa-play::before, .fa-chevron-left::before ,.fa-th-list, .fa-eye-slash {color:`+blue+` !important} .Emision, .CapNv, .Rprt, .CVst, .Xpnd {background-color:`+grey+`;color:`+white+`;} .Emision a:hover {color:`+blue+` !important} @media (min-width: 1020px) {.CpCn.show .CpCnA:before, .CpCn.show .CpCnA:after {background-color:`+black+`}}`
+    let Style = `.Header,.Footer {background-color:`+grey+`;} #search-anime, h1, h2, a, strong { color:`+white+` !important;} .Body, .WdgtCn, .WdgtCn .Title {background-color:`+black+`;color:`+white+`;} .CapiTnv>li>a {background-color:`+lgrey+`;} .CapiTnv>li.active>a {background-color:`+grey+`;box-shadow: inset 0 2px 0 `+blue+`;} .fa-play::before, .fa-chevron-left::before ,.fa-th-list, .fa-eye-slash {color:`+blue+` !important} .Emision, .CapNv, .Rprt, .CVst, .Xpnd {background-color:`+grey+`;color:`+white+`;} .Emision a:hover {color:`+blue+` !important} @media (min-width: 1020px) {.CpCn.show .CpCnA:before, .CpCn.show .CpCnA:after {background-color:`+black+`}} .fa-facebook::before{color=`+fb+`;} .fa-youtube::before{color=`+red+`;}`
     let sS = document.createElement("style")
     sS.setAttribute("type", "text/css");
     sS.innerText = Style
