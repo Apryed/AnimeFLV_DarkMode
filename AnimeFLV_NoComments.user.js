321551809d2d10cb006e0dca2515aee2687c29d9
Apryed <Apryed@users.noreply.github.com>
2025-02-13 19:21:50 +0100

    Update AnimeFLV_NoComments.user.js

--- AnimeFLV_NoComments.user.js
+++ AnimeFLV_NoComments.user.js
@@ -6,8 +6,8 @@
 // @author			Apryed
 // @match			https://www3.animeflv.net/ver/*
 // @icon			https://www.google.com/s2/favicons?sz=64&domain=animeflv.net
-// @downloadURL		https://raw.githubusercontent.com/Apryed/AnimeFLV_DarkMode/main/AnimeFLV.user.js
-// @updateURL		https://raw.githubusercontent.com/Apryed/AnimeFLV_DarkMode/main/AnimeFLV.user.js
+// @downloadURL		https://raw.githubusercontent.com/Apryed/AnimeFLV_DarkMode/main/AnimeFLV_NoComments.user.js
+// @updateURL		https://raw.githubusercontent.com/Apryed/AnimeFLV_DarkMode/main/AnimeFLV_NoComments.user.js
 // @supportURL		https://github.com/Apryed/AnimeFLV_DarkMode/issues
 // @run-at			document-end
 // @grant			none
