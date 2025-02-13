aacdc2032869d1738a95ed3861d6943f33c9c8bb
Apryed <Apryed@users.noreply.github.com>
2025-02-13 19:19:44 +0100

    Create AnimeFLV_NoComments.user.js

---
+++ AnimeFLV_NoComments.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name			AnimeFLV - Comments Removal
+// @namespace		https://github.com/Apryed/AnimeFLV_DarkMode
+// @version			0.0.1
+// @description		Removes AnimeFLV Comment section - Will try to remove everything from Disqus in due time
+// @author			Apryed
+// @match			https://www3.animeflv.net/ver/*
+// @icon			https://www.google.com/s2/favicons?sz=64&domain=animeflv.net
+// @downloadURL		https://raw.githubusercontent.com/Apryed/AnimeFLV_DarkMode/main/AnimeFLV.user.js
+// @updateURL		https://raw.githubusercontent.com/Apryed/AnimeFLV_DarkMode/main/AnimeFLV.user.js
+// @supportURL		https://github.com/Apryed/AnimeFLV_DarkMode/issues
+// @run-at			document-end
+// @grant			none
+// ==/UserScript==
+
+(function () {
+	'use strict';
+		document.querySelectorAll("div.WdgtCn[style='background: rgb(0, 0, 0);'")[1].remove()
+	}
+})();
